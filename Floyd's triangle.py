f7ac119b2115ad40967589f7b9eb6a865f4d488d
Geniusguy24 <ekanshpardeshi2412@gmail.com>
2025-03-20 19:37:57 +0530

    Floyd's Triangle

---
+++ Floyd's triangle.py
@@ -0,0 +1,9 @@
+print("Floyd's Triangle:")
+rows = int(input("Please Enter number of rows: "))
+number = 1
+
+for i in range(1, rows + 1):
+    for j in range(1, i + 1):
+        print(number, end= " ")
+        number = number + 1
+    print()
